0985c3ae569c8e1efdda7061c74b273c94e6a40c
Srebrna01 <maja.lipska@gmail.com>
2022-08-04 21:20:30 +0200

    Dokończony kod 1 oraz przeprowadzone testy komend

--- 2022_4_1.py
+++ 2022_4_1.py
@@ -1,9 +1,23 @@
 #Zadanie 4.1 Matura 2022
-y = str(101)
-print(y)
-print (y[-1])
 
-if y[0] == y[-1]:
-    print ("sukces")
-else:
-    print ("not working")
+f = open("matura2022/liczby.txt", "r")
+
+anwser = []
+
+
+for x in f:
+    x = str(x).rstrip()
+    x = str(x).lstrip()
+    if str(x)[0] == str(x)[-1]:
+      anwser.append(x)    
+      continue
+    else :
+      print (" ")
+
+length = len(anwser)
+
+length = str(length)
+    
+anwser = anwser[0]
+
+print ("The final anwser is ", length , "and " , anwser)
